738ccfe9700e2d624f2e93f9b5f179e18afeb80e
alfredking12 <alfredking12@163.com>
2016-08-27 11:35:44 +0800

    敏感信息保护

--- dcenter/src/config/config.js
+++ dcenter/src/config/config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    db_connection_string: "mysql://dev:" + encodeURIComponent('Bysun4321$#@!') + "@120.26.239.94:3306/hp_scheduler",
+
+    db_connection_string: "mysql://localhost:3306/hp_scheduler",
     db_pool_max: 2,
     db_pool_min: 0,
     db_pool_idle: 10000,
 
-    rabbitmq_url: 'amqp://mquser:Bysun@120.26.242.51:5672',
+    rabbitmq_url: 'amqp://localhost:5672',
 
-    max_page_size: 200
+    max_page_size: 200,
 }

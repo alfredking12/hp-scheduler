a4274405d270218d99f0e1773fa82063d3d36d63
alfredking12 <alfredking12@163.com>
2016-08-31 16:05:25 +0800

    dev

--- console/src/app/config/config.js
+++ console/src/app/config/config.js
@@ -2,7 +2,7 @@ var config = {
     api_server: 'http://localhost:9001',
 }
 
-//config = require('./config.dev');
+config = require('./config.dev');
 //config = require('./config.sit');
 //config = require('./config.pre');
 //config = require('./config.prd');
